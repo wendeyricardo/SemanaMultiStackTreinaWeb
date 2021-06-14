3784e669a3950da80ae6211ca5bf265665c3c399
wendey <wendeyricardo@gmail.com>
2021-06-14 00:42:55 -0300

    criando o componente de ambiente seguro - safeEnvironment

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
 
 export default function Home() {
-  return  <div></div>
-  //teste
+  return  (
+  <div>
+    <SafeEnvironment />
+  </div>
+  );  
 }
