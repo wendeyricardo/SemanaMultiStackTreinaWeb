f57fca20f67a383c40c011d2f111cfbe5ad3aa42
wendey <wendeyricardo@gmail.com>
2021-06-14 20:00:09 -0300

    corrigindo o componente header

--- ediaristas/src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++ ediaristas/src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -4,6 +4,6 @@ export const SafeEnvironmentContainer = styled("div")`
   color: ${({ theme }) => theme.palette.text.secondary};
   background-color: ${({ theme }) => theme.palette.background.default};
   text-align: right;
-  padding: ${({ theme }) => theme.spacing(2)}0;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
   font-size: 12px;
 `;
