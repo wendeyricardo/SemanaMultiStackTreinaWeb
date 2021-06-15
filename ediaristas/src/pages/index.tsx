af1ee18cea57788d3072ce979e4c5901514de00c
wendey <wendeyricardo@gmail.com>
2021-06-15 02:52:35 -0300

    criando o componente userInformation

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,15 +1,24 @@
-import Head from 'next/head'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
-  return  (
-  <div>
-    <SafeEnvironment />
-    <PageTitle 
-      title={'Conheça os profissionais'}
-      subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+  return (
+    <div>
+      <SafeEnvironment />
+      <PageTitle
+        title={"Conheça os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
       />
-  </div>
-  );  
+
+      <UserInformation
+        name={"Wendey Ricardo"}
+        picture={"https://github.com/wendeyricardo.png"}
+        rating={3}
+        description={"benfica"}
+      />
+    </div>
+  );
 }
