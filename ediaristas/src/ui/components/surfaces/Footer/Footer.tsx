57899e046dafda704b447c94f176bc04b0f83d6e
wendey <wendeyricardo@gmail.com>
2021-06-16 16:26:45 -0300

    criando o componente de rodapé

---
+++ ediaristas/src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <FooterStyled sx={{ maxWidth: "400px" }}>
+      <FooterContainer>
+        <Box>
+          <FooterTitle>Quem somos</FooterTitle>
+          <Typography variant={"body2"} sx={{ marginTop: "16px" }}>
+            E-Diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos a melhor profissional com
+            total segurança e praticidade! São milhares de clientes satisfeitos
+            por todo o país.
+          </Typography>
+        </Box>
+        <div>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"nooperner noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"nooperner noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google play"} />
+              </a>
+            </li>
+          </AppList>
+        </div>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
