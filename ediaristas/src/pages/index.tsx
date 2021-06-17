675733c5edeb282ec4bd0eb9e4797dd5fc14738b
wendey <wendeyricardo@gmail.com>
2021-06-17 20:48:08 -0300

    criando o componente de input cep

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
 
 export default function Home() {
   return (
@@ -11,6 +12,12 @@ export default function Home() {
         subtitle={
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
+      />
+      <TextFieldMask
+        mask={"99.999-99"}
+        label={"digite seu CEP"}
+        fullWidth
+        variant={"outlined"}
       />
 
       <UserInformation
