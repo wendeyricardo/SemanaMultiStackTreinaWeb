19371062e2d0ef632b78ed10c45451564e9ef6fc
wendey <wendeyricardo@gmail.com>
2021-06-18 00:37:29 -0300

    ultimos ajustes nos componentes do front-end

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,10 +2,11 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
-import { Button, Typography } from "@material-ui/core";
+import { Button, Typography, Container } from "@material-ui/core";
 import {
   FormElementsContainer,
   ProfissionaisPaper,
+  ProfissionaisContainer,
 } from "ui/styles/pages/index.style";
 
 export default function Home() {
@@ -18,32 +19,66 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <FormElementsContainer>
-        <TextFieldMask
-          mask={"99.999-99"}
-          label={"digite seu CEP"}
-          fullWidth
-          variant={"outlined"}
-        />
-        <Typography color={"error"}>CEP inválido</Typography>
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-99"}
+            label={"digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
 
-        <Button
-          variant={"contained"}
-          color={"secondary"}
-          sx={{ width: "220px" }}
-        >
-          Buscar
-        </Button>
-      </FormElementsContainer>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
 
-      <ProfissionaisPaper>
-        <UserInformation
-          name={"Wendey Ricardo"}
-          picture={"https://github.com/wendeyricardo.png"}
-          rating={3}
-          description={"benfica"}
-        />
-      </ProfissionaisPaper>
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+            <UserInformation
+              name={"Wendey Ricardo"}
+              picture={"https://github.com/wendeyricardo.png"}
+              rating={3}
+              description={"benfica"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
