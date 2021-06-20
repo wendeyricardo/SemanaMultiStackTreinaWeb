333572b6bc756e9621b7db4638cba600302199e5
wendey <wendeyricardo@gmail.com>
2021-06-20 01:33:52 -0300

    criando a api service

--- ediaristas/src/pages/index.tsx
+++ ediaristas/src/pages/index.tsx
@@ -2,7 +2,12 @@ import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironm
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 import TextFieldMask from "ui/components/inputs/TextFieldMask/TextFieldMask";
-import { Button, Typography, Container } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  Container,
+  CircularProgress,
+} from "@material-ui/core";
 import {
   FormElementsContainer,
   ProfissionaisPaper,
@@ -11,7 +16,17 @@ import {
 import useIndex from "data/hooks/pages/useIndex.page";
 
 export default function Home() {
-  const { cep, setCep, cepValido } = useIndex();
+  const {
+    cep,
+    setCep,
+    cepValido,
+    buscarProfissionais,
+    erro,
+    diaristas,
+    buscaFeita,
+    carregando,
+    diaristasRestantes,
+  } = useIndex();
 
   return (
     <div>
@@ -32,58 +47,61 @@ export default function Home() {
             value={cep}
             onChange={(event) => setCep(event.target.value)}
           />
-          {cepValido}
-          <Typography color={"error"}>CEP inválido</Typography>
+
+          {erro && <Typography color={"error"}>{erro}</Typography>}
 
           <Button
             variant={"contained"}
             color={"secondary"}
             sx={{ width: "220px" }}
+            disabled={!cepValido || carregando}
+            onClick={() => buscarProfissionais(cep)}
           >
-            Buscar
+            {carregando ? <CircularProgress size={20} /> : "Buscar"}
           </Button>
         </FormElementsContainer>
-
-        <ProfissionaisPaper>
-          <ProfissionaisContainer>
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-            <UserInformation
-              name={"Wendey Ricardo"}
-              picture={"https://github.com/wendeyricardo.png"}
-              rating={3}
-              description={"benfica"}
-            />
-          </ProfissionaisContainer>
-        </ProfissionaisPaper>
+        {buscaFeita && (
+          <ProfissionaisPaper>
+            <ProfissionaisContainer>
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+              <UserInformation
+                name={"Wendey Ricardo"}
+                picture={"https://github.com/wendeyricardo.png"}
+                rating={3}
+                description={"benfica"}
+              />
+            </ProfissionaisContainer>
+          </ProfissionaisPaper>
+        )}
       </Container>
     </div>
   );
